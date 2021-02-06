33b2d0035d3321eb00ef3087a7af5a34ae1fb976
Jason <jkasari@outlook.com>
2021-02-06 13:31:34 -0800

    More or less set up the |queen.h| file

--- queen.h
+++ queen.h
@@ -1,12 +1,35 @@
+#include <vector>
 #ifndef queen_h
 #define queen_h
+
+using Position = std::array<uint32_t, 2>;
+
 
 class queen {
 
  public:
+  queen(uint32_t, uint32_t);
+
+  /**
+   * Returns a value that represents how many other |queen|s are
+   * a |queen| from any given location.
+   */
+  uint32_t underAttackFrom(uint32_t, uint32_t);
+
+  /**
+   * Returns the |row| of a |queen|.
+   */
+  uint32_t row();
+
+  /**
+   * Returns the |col| of a |queen|.
+   */
+  uint32_t col();
 
  private:
 
+  uint32_t currentRow;
+  uint32_t currentCol;
 };
 
 #endif // queen_h
--- test.cpp
+++ test.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 #include "nuttiest/nuttiest.hpp"
+#include "queen.h"
 
 using namespace std;
 using namespace nuttiest;
 
 int main() {
-  section("test") {
-    unit_test("testCase") {
-      assert_eq(true, false);
+  section("row") {
+    unit_test("A queen on row 4") {
+      queen hearts(4, 5);
+      assert_eq(4, hearts.row());
     }
   }
 
