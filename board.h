a566bb72b1c052f401d99d871f3f6968c3525561
Jason <jkasari@outlook.com>
2021-02-06 14:13:52 -0800

    Realized the queen needed a move function, so I did that

--- board.h
+++ board.h
@@ -1,11 +1,19 @@
+#include "queen.h"
 #ifndef board_h
 #define board_h
 
-class board {
+class Board {
 
  public:
 
+  /**
+   * Finds the best possible move for any given queen
+   */
+  Queen findBestMove(Queen);
+
  private:
+  static const int32_t boardSize = 8;
+  char chessBoard[boardSize][boardSize];
 
 };
 
--- test.cpp
+++ test.cpp
@@ -6,6 +6,18 @@ using namespace std;
 using namespace nuttiest;
 
 int main() {
+  section("moveTo") {
+    unit_test("Move a queen around") {
+      Queen hearts(0, 0);
+      hearts.moveTo(4,0);
+      assert_eq(4, hearts.row());
+      assert_eq(0, hearts.col());
+
+      hearts.moveTo(4, 7);
+      assert_eq(4, hearts.row());
+      assert_eq(7, hearts.col());
+    }
+  }
   section("row") {
     unit_test("Queen at a current row") {
       Queen hearts(0, 5);
