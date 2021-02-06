f8c5bfd3af60e0cb0255692aaa305d1d4dadd259
Jason <jkasari@outlook.com>
2021-02-06 13:42:25 -0800

    "Create the |queen| class along with basic tests"
    Really unsure what all the queen is going to need to do so I'm sure
    I will revisit this. Right now the queen just stores a location and
    thats literally it.

--- queen.h
+++ queen.h
@@ -5,16 +5,10 @@
 using Position = std::array<uint32_t, 2>;
 
 
-class queen {
+class Queen {
 
  public:
-  queen(uint32_t, uint32_t);
-
-  /**
-   * Returns a value that represents how many other |queen|s are
-   * a |queen| from any given location.
-   */
-  uint32_t underAttackFrom(uint32_t, uint32_t);
+  Queen(uint32_t, uint32_t);
 
   /**
    * Returns the |row| of a |queen|.
--- test.cpp
+++ test.cpp
@@ -7,9 +7,19 @@ using namespace nuttiest;
 
 int main() {
   section("row") {
-    unit_test("A queen on row 4") {
-      queen hearts(4, 5);
-      assert_eq(4, hearts.row());
+    unit_test("Queen at a current row") {
+      Queen hearts(0, 5);
+      Queen spades(4, 5);
+      assert_eq(0, hearts.row());
+      assert_eq(4, spades.row());
+    }
+  }
+  section("col") {
+    unit_test("Queen at a current col") {
+      Queen hearts(5, 0);
+      Queen spades(5, 4);
+      assert_eq(0, hearts.col());
+      assert_eq(4, spades.col());
     }
   }
 
