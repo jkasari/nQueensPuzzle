cdf56fc5f5d0297c46c9ed7a4a6010e5c4a39ca9
Jason <jkasari@outlook.com>
2021-02-06 14:30:01 -0800

    Add function names in board.h

--- board.cpp
+++ board.cpp
@@ -1,1 +1,28 @@
 #include "board.h"
+#include "queen.h"
+#include <iomanip>
+
+std::ostream& operator<<(std::ostream& stream, const Board& chessBoard) {
+    stream << "+---+---+---+---+---+---+---+---+" << std::endl;
+    for(int i = 0; i < Board::boardSize; i++) {
+        stream << "| ";
+        for(int j = 0; j < Board::boardSize; j++) {
+        //    stream << std::setfill('0') << std::setw(2);
+            stream << static_cast<char>(chessBoard.chessBoard[i][j]) << " | ";
+        }
+        if(i < Board::boardSize - 1) {
+          stream << std::endl << "|---|---|---|---|---|---|---|---|" << std::endl;
+        }
+    }
+    stream << std::endl << "+---+---+---+---+---+---+---+---+" << std::endl;
+    return stream;
+}
+
+
+Board::Board() {
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j < 8; j++) {
+            chessBoard[i][j] = ' ';
+        }
+    }
+}
--- board.h
+++ board.h
@@ -4,14 +4,29 @@
 
 class Board {
 
+  friend std::ostream& operator<<(std::ostream&, const Board&);
+
  public:
+  Board();
+
+
+  /**
+   * This goes over all the queens in play and populates the
+   * board with them/
+   */
+  void update(std::vector<Queen>);
 
   /**
    * Finds the best possible move for any given queen
    */
   Queen findBestMove(Queen);
 
+
  private:
+  /**
+   * This leaves an '#' logo where a queen currently is.
+   */
+  void markBoard(Queen);
   static const int32_t boardSize = 8;
   char chessBoard[boardSize][boardSize];
 
