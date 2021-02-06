247822d99562823909a760ba1c83f2b983c3aa81
Jason <jkasari@outlook.com>
2021-02-06 13:03:13 -0800

    Add the needed classes for the puzzle |board| |queen|

---
+++ board.h
@@ -0,0 +1,12 @@
+#ifndef board_h
+#define board_h
+
+class board {
+
+ public:
+
+ private:
+
+};
+
+#endif // board_h
---
+++ queen.h
@@ -0,0 +1,12 @@
+#ifndef queen_h
+#define queen_h
+
+class queen {
+
+ public:
+
+ private:
+
+};
+
+#endif // queen_h
