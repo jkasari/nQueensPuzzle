e282ef75b72db9a234e744488ee60520ca9212a2
Jason <jkasari@outlook.com>
2021-02-13 19:53:27 -0800

    Wrote |displayBoard|

--- board.cpp
+++ board.cpp
@@ -1,13 +1,14 @@
 #include "board.h"
 #include <iomanip>
+#include <iostream>
 
-std::ostream& operator<<(std::ostream& stream, const Board& chessBoard) {
+std::ostream& operator<<(std::ostream& stream, const Board& displayChessBoard) {
   stream << "+---+---+---+---+---+---+---+---+" << std::endl;
   for (int i = 0; i < Board::boardSize; i++) {
     stream << "| ";
     for (int j = 0; j < Board::boardSize; j++) {
       //    stream << std::setfill('0') << std::setw(2);
-      stream << static_cast<char>(chessBoard.chessBoard[i][j]) << " | ";
+      stream << static_cast<char>(displayChessBoard.displayChessBoard[i][j]) << " | ";
     }
     if (i < Board::boardSize - 1) {
       stream << std::endl << "|---|---|---|---|---|---|---|---|" << std::endl;
@@ -20,7 +21,24 @@ std::ostream& operator<<(std::ostream& stream, const Board& chessBoard) {
 Board::Board() {
   for (int i = 0; i < 8; i++) {
     for (int j = 0; j < 8; j++) {
-      chessBoard[i][j] = ' ';
+      displayChessBoard[i][j] = ' ';
+      chessBoard[1][j] = true;
     }
   }
+}
+
+void Board::displayBoard(void) {
+  uint32_t row = 0;
+  uint32_t col = 0;
+  while(isOnBoard(row, col)) {
+    while(isOnboard(row, col)) {
+      if(chessBoard[row][col]) {
+        displayChessBoard[row][col] = '@';
+      }
+      col += 1;
+    }
+    col = 0;
+    row += 1;
+  }
+  std::cout << displayChessBoard << std::endl;
 }
