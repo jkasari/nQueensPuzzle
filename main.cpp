ab83570dbeb5b65838458ae6b6999aa59c8f2eed
Jason <jkasari@outlook.com>
2021-02-14 21:01:41 -0800

    Make Jason happy with the program
    I'm happy with this version. It compiles and runs. Has error codes. It
    still needs a fair amount of refactorying. But as of this commit, it is
    a complete working program.

--- main.cpp
+++ main.cpp
@@ -6,9 +6,28 @@ using namespace std;
 
 
 int main(int argc, char** argv) {
+  if(argc != 2) {
+    cout << endl;
+    cout << "Just give the size of the board you want to calculate, nothing more, nothing less.";
+    cout << endl << endl;
+    return 0;
+  }
   uint32_t sizeOfBoard = stoul(argv[1]);
+  if(sizeOfBoard < 4) {
+    cout << endl;
+    cout << "Sorry partner, anything less then a 4 X 4 just don't work";
+    cout << endl << endl;
+    return 0;
+  } else if( sizeOfBoard > 42) {
+    cout << endl;
+    cout << "Calm youself!! This isn't a super computer! You try solving with a board bigger then 42!";
+    cout << endl << endl;
+    return 0;
+  }
   Board chessBoard(sizeOfBoard);
   chessBoard.placeQueens(0);
   cout << endl << chessBoard << endl;
+  cout << "If apsolute power corrupts apsolutely, does absolute powerlessness make you pure?";
+  cout << endl << endl;
   return 0;
 }
